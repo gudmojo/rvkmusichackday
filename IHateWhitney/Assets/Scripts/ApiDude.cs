1a3931709670444126e0384a6f4b1477f843a5be
Gudmundur Johannsson <gudmundur.orn@gmail.com>
2012-10-27 20:57:47 +0000

    reading image url

--- IHateWhitney/Assets/Scripts/ApiDude.cs
+++ IHateWhitney/Assets/Scripts/ApiDude.cs
@@ -4,24 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using MiniJSON;
  
-namespace Assets.Scripts {
 public class ApiDude : MonoBehaviour {
-    HashSet<Song> songs = new HashSet<Song>();
+    Dictionary<string, Song> songs = new Dictionary<string, Song>();
 
   void Start () {
-		//http://developer.echonest.com/api/v4/song/search?api_key=CWXFYIFQ9HXXTW1K3&sort=song_hotttnesss-desc&bucket=song_hotttnesss&results=10
-             //search by name
-            //http://api.7digital.com/1.2/track/search?q=rihanna%20where%20have%20you%20been&y&oauth_consumer_key=7d4vqmmnvjrt&country=GB&pagesize=2
-            //get preview
-            //http://api.7digital.com/1.2/track/preview?trackid=10353559&oauth_consumer_key=7d4vqmmnvjrt
-
-            //get image
-
    StartCoroutine("GetChart");     
   }
  
   IEnumerator GetChart() {
-      WWW www = new WWW("http://developer.echonest.com/api/v4/song/search?api_key=CWXFYIFQ9HXXTW1K3&sort=song_hotttnesss-desc&bucket=song_hotttnesss&results=10");
+      WWW www = new WWW("http://developer.echonest.com/api/v4/song/search?api_key=CWXFYIFQ9HXXTW1K3&sort=song_hotttnesss-desc&bucket=song_hotttnesss&results=100");
     
     float elapsedTime = 0.0f;
     
@@ -45,17 +36,24 @@ public class ApiDude : MonoBehaviour {
     IDictionary search = (IDictionary) Json.Deserialize(response);
 
     IDictionary response2 = (IDictionary)search["response"];
-    Debug.Log(response2.GetType());
     IList songsresponse = (IList)response2["songs"];
     int i = 1;
     foreach (IDictionary s in songsresponse) {
         Song song = new Song();
         song.Artist = (string) s["artist_name"];
         song.Title = (string) s["title"];
-        song.ChartPosition = i++;
         song.Init();
-        songs.Add(song);
-    } 
+        if (!songs.ContainsKey(song.Key))
+        {
+            song.ChartPosition = i++;
+            song.FetchPreviewUrl();
+            songs.Add(song.Key, song);
+        }
+    }
+
+    foreach (Song song in songs.Values)
+    {
+        Debug.Log(song.Key + " : " + song.ImageUrl);
+    }
   }   
-}
 }
--- IHateWhitney/Assets/Scripts/Song.cs
+++ IHateWhitney/Assets/Scripts/Song.cs
@@ -4,33 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Net;
 
-namespace Assets.Scripts
+class Song
 {
-    class Song
+    public string Key; //a string that identifies the song. Usually Artist - Title
+    public string Artist;
+    public string Title;
+    public int ChartPosition;
+    public string PreviewUrl;
+    public string ImageUrl;
+
+    public void Init()
     {
-        public string Artist;
-        public string Title;
-        public string ChartPosition;
-        public string PreviewUrl;
+        Key = Artist + " - " + Title;
+    }
 
-        public void Init()
-        {
-            FetchPreviewUrl();
-        }
+    public void FetchPreviewUrl()
+    {
+        string URI = "http://api.7digital.com/1.2/track/search?q=" + Artist + " " + Title + "&y&oauth_consumer_key=7d4vqmmnvjrt&country=GB&pagesize=2";
+        WebClient webClient = new WebClient();
+        string strXml = webClient.DownloadString(URI);
+        int ix = strXml.IndexOf("track id=\"") + 10;
+        string track_id = strXml.Substring(ix).Split('"')[0];
 
-        public void FetchPreviewUrl()
-        {
-            string URI = "http://api.7digital.com/1.2/track/search?q=" + Artist + " " + Title + "&y&oauth_consumer_key=7d4vqmmnvjrt&country=GB&pagesize=2";
-            WebClient webClient = new WebClient();
-            string strXml = webClient.DownloadString(URI);
-            int ix = strXml.IndexOf("track id=\"") + 10;
-            string track_id = strXml.Substring(ix).Split('"')[0];
-            //search by name
-            //http://api.7digital.com/1.2/track/search?q=rihanna%20where%20have%20you%20been&y&oauth_consumer_key=7d4vqmmnvjrt&country=GB&pagesize=2
-            //get preview
-            //http://api.7digital.com/1.2/track/preview?trackid=10353559&oauth_consumer_key=7d4vqmmnvjrt
+        PreviewUrl = "http://api.7digital.com/1.2/track/preview?trackid=" + track_id + "&oauth_consumer_key=7d4vqmmnvjrt";
 
-            PreviewUrl = track_id;
-        }
+        ix = strXml.IndexOf("<image>") + 7;
+        ImageUrl = strXml.Substring(ix).Split('<')[0].Replace(".jpg", "0.jpg");
+
     }
 }
